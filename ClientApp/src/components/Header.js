8b733256a4a60c9f988830bc4082a4faa4647624
nikosomos <somos@ualberta.ca>
2021-09-13 21:59:51 -0600

    fixed logo redirect

--- ClientApp/src/components/Header.js
+++ ClientApp/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Button, Toolbar } from "@material-ui/core";
 import { ReactComponent as TheScoreLogo } from "public/Logo.svg";
 import { HeaderStyles as getStyles } from "./styles";
 import { useHistory } from "react-router";
@@ -11,10 +11,9 @@ export default function Header() {
   return (
     <AppBar className={classes.appBar} position="static">
       <Toolbar className={classes.toolBar}>
-        <TheScoreLogo
-          className={classes.logo}
-          onClick={() => history.push("https://www.thescore.com/")}
-        />
+        <Button href="https://www.thescore.com/">
+          <TheScoreLogo className={classes.logo} />
+        </Button>
       </Toolbar>
     </AppBar>
   );
